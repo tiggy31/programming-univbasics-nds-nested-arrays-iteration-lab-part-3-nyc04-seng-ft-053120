85391c3c12d4b1faaf4bdb893092104252667668
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-10 03:20:17 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,9 @@
+      require "pry"
 def join_nested_strings(src)
-  # src will be an Array of Arrays of Strings and Integers
+               
+src.flatten.grep(String).join(' ')     
+
+ end
+ 
+# src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-end
